0ae0187a2935360967235d187e132dde24e9dd91
Blake Mason <blakelmason@gmail.com>
2019-06-04 13:50:58 -0700

    CSS and HTML loaders.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,15 +2,48 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        main: "./src/main.js"
+        main: './src/main.js'
     },
-    mode: "development",
+    mode: 'development',
     output: {
-        filename: "[name]-bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-        publicPath: "/"
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/'
     },
     devServer: {
-        contentBase: "dist"
+        contentBase: 'dist',
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].html'
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
     }
-}
+};
