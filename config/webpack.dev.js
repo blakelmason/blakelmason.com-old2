92da840778a3a5fc2a5196b6851f5016d76edf94
blakelmason <blakelmason@gmail.com>
2019-06-07 22:35:13 -0700

    Babel config cleanup of dev dependencies,

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,60 +1,69 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    main: "./src/main.js"
-  },
-  mode: "development",
-  output: {
-    filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
-  },
-  devServer: {
-    contentBase: "dist",
-    overlay: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].html"
-            }
-          },
-          {
-            loader: "extract-loader"
-          },
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|gif|png)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "images/[name]-[hash:8].[ext]"
-            }
-          }
-        ]
-      }
-    ]
-  }
+    entry: {
+        main: ['./src/main.js'],
+    },
+    mode: 'development',
+    output: {
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        contentBase: 'dist',
+        overlay: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].html',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader',
+                    },
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name]-[hash:8].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
