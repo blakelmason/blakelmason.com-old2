721b08c95222a803ea694a8349c10a13e9d36c86
Blake Mason <blakelmason@gmail.com>
2019-06-10 15:46:10 -0700

    Removed src.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-require('babel-runtime/regenerator')
-require('react-hot-loader/patch')
-require('babel-register')
-require('webpack-hot-middleware/client?reload=true')
-require('./main.css')
-require('./images/link.jpg')
-require('./index.html')
-require('./app.js')
