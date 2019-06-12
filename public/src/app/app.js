9cfed72b2065bdaf75a313a84f1ea67e06cbabb0
Blake Mason <blakelmason@gmail.com>
2019-06-12 14:33:09 -0700

    React hot reloading and basic layout setup.

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -1,25 +1,33 @@
+import { hot } from 'react-hot-loader/root'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './app.scss'
 
 class App extends Component {
-   constructor() {
-      super()
-      this.state = {
-         title: 'Heyo77777!',
-      }
-   }
    render() {
       return (
-         <div>
-            {this.state.title}
-            <div>Test2</div>
+         <div className="h-100">
+            <div className="container p-0 h-100">
+               <div className="row no-gutters h-100">
+                  <div className="col h-100 px-3">
+                     <div
+                        className="p-3 h-100 text-light"
+                        style={{ WebkitTextStroke: '2px black' }}
+                     >
+                        <h1 style={{ fontSize: '3.5rem', fontWeight: 'bold' }}>
+                           Blake Mason - Web Portfolio
+                        </h1>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       )
    }
 }
 
-export default App
+export default hot(App)
 
 const root = document.getElementById('root')
 ReactDOM.render(<App />, root)
