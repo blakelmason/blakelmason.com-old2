664692e71458ad2ac8b46b0e7f714623843025b0
blakelmason <blakelmason@gmail.com>
2019-06-12 21:25:47 -0700

    Background styling fix. LAN webpack-dev-server configuration.

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -6,73 +6,154 @@ import './app.scss'
 import Tool from './components/Tool'
 
 const toolList = [
-   ['HTML', 'https://www.w3schools.com/html/html_intro.asp'],
-   ['CSS', 'https://www.w3schools.com/html/html_css.asp'],
-   ['Bootstrap', 'https://getbootstrap.com/'],
-   ['jQuery'],
-   ['Handlebars.js'],
-   ['Node.js'],
-   ['Express.js'],
-   ['MongoDB'],
-   ['Mongoose'],
-   ['SQL'],
-   ['MySQL'],
-   ['PostgreSQL'],
-   ['Sequelize'],
-   ['React'],
-   ['React-Router'],
-   ['Redux'],
-   ['Moment.js'],
-   ['Cheerio'],
-   ['JWT'],
-   ['Webpack'],
-   ['Babel'],
-   ['Heroku'],
+  ['HTML', 'https://www.w3schools.com/html/html_intro.asp'],
+  ['CSS', 'https://www.w3schools.com/html/html_css.asp'],
+  ['Bootstrap', 'https://getbootstrap.com/'],
+  ['jQuery'],
+  ['Handlebars.js'],
+  ['Node.js'],
+  ['Express.js'],
+  ['MongoDB'],
+  ['Mongoose'],
+  ['SQL'],
+  ['MySQL'],
+  ['PostgreSQL'],
+  ['Sequelize'],
+  ['React'],
+  ['React-Router'],
+  ['Redux'],
+  ['Moment.js'],
+  ['Cheerio'],
+  ['JWT'],
+  ['Webpack'],
+  ['Babel'],
+  ['Heroku'],
 ]
 
 class App extends Component {
-   render() {
-      return (
-         <div>
-            <div className="container">
-               <div className="row">
-                  <div className="col">
-                     <h1
-                        style={{
-                           background: 'rgb(255,255,255, .8',
-                        }}
-                        className="my-5 bg-dark text-white border border-secondary rounded d-inline-block px-3 pt-2 pb-2"
-                     >
-                        Blake Mason - Web Portfolio
-                     </h1>
-                  </div>
-               </div>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="my-5 text-white pt-2 pb-2 display-4">
+                Blake Mason - Web Portfolio
+              </div>
             </div>
-            <div className="container">
-               <div className="row">
-                  <div className="col">
-                     <div
-                        style={{
-                           display: 'flex',
-                           flexWrap: 'wrap',
-                        }}
-                     >
-                        <div
-                           className="text-white bg-dark rounded border border-secondary px-2 d-flex justify-content-center align-items-center my-1 mr-2"
-                           style={{ height: 31 }}
-                        >
-                           <strong>Tools :</strong>
-                        </div>
-                        {toolList.map(tool => (
-                           <Tool text={tool[0]} link={tool[1]} key={tool[0]} />
-                        ))}
-                     </div>
-                  </div>
-               </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div
+                style={{
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  flex: 1,
+                  overflowY: 'auto',
+                }}
+              >
+                <div
+                  className="text-white h5 px-2 d-flex justify-content-center align-items-center my-1 mr-2"
+                  style={{ height: 31 }}
+                >
+                  Tools :
+                </div>
+                {toolList.map(tool => (
+                  <Tool text={tool[0]} link={tool[1]} key={tool[0]} />
+                ))}
+              </div>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
             </div>
-         </div>
-      )
-   }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default hot(App)
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,37 +1,42 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ip = require('ip')
 
 module.exports = {
-   module: {
-      rules: [
-         {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-               loader: 'babel-loader',
-            },
-         },
-         {
-            test: /\.html$/,
-            use: [
-               {
-                  loader: 'html-loader',
-               },
-            ],
-         },
-         {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-         },
-      ],
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         template: './src/index.html',
-         filename: './index.html',
-      }),
-   ],
+  devServer: {
+    host: ip.address(),
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 }
