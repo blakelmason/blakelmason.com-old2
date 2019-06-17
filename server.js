2dba5bb864533bbe8f641afaf11f31d734927fd8
Blake Mason <blakelmason@gmail.com>
2019-06-17 16:09:27 -0700

    Heroku setup.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express'
+import path from 'path'
 
 let server = express()
 
 const PORT = process.env.PORT || 3000
 
-server.get('/', (req, res) => res.send('HELLO FROM EXPRESS'))
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname + '/client/dist')))
 
-process.env.NODE_ENV === 'production' && server.use(express.static('public'))
+  server.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'))
+  )
+} else {
+  server.get('/', (req, res) => res.send('HELLO FROM EXPRESS'))
+}
 
 server.listen(3000, () => console.log(`Listening on port ${PORT}`))
